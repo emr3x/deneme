906244b5db2c058daa61c609be1d9863a2e57818
Emre Ates <orhan.emre.ates8@gmail.com>
2023-02-18 16:13:28 +0300

    commit msg

--- Emre files/emre.js
+++ Emre files/emre.js
@@ -3,4 +3,5 @@ const numbers2 = [12, 14, 15, 18];
 const result = numbers2.filter((number) => number > 10);
 console.log(numbers2.filter((number) => number("4")));
 console.log("check");
+console.log("check2");
 const frnrmr = 22;
